56f71ab3f71856cb5b8c58c17e60ac4e4d8fad88
Sarah Huchendorf <sarah.coding@outlook.com>
2021-09-08 16:03:36 -0700

    week2homeworkdone

---
+++ Week2Homework/Week2.js
@@ -0,0 +1,21 @@
+const pizzaPlace = "Nickis Pizza";
+let numberOfToppings = 8;
+
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+console.log(pizzaPlace + " " + numberOfToppings);
+
+
+
+console.log(`"At ${pizzaPlace}, we have ${numberOfToppings} toppings."`)
+
+
+
+if (numberOfToppings <10)  {
+console.log("Quality, not Quantity.")
+} else {
+console.log("A whole lot of pizza")
+}
+
+
+
